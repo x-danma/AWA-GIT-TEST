7084fbf2b9b09c8b3c57b7ff3eb9e2a132610145
Emelie Orraryd <emelie.orraryd@gmail.com>
2015-09-24 11:23:18 +0200

    Added math of Addtwonumbers

--- GITTest/GITTest/Program.cs
+++ GITTest/GITTest/Program.cs
@@ -22,9 +22,15 @@ namespace GITTest
             
 
             int b = 7;
+            int myInt = AddTwoNumbers(b, a);
 
-            Console.WriteLine(Convert.ToString(AddTwoNumbers(a,b)));
+            Console.WriteLine(Convert.ToString(myInt));
 
+
+            double rootNumber = Math.Sqrt(myInt);
+
+            Console.WriteLine(Convert.ToString(rootNumber));
+            
         }
 
        static public int AddTwoNumbers(int x, int z)
